782f31a74824a59a9637801962a102d50f44fe35
bhuwansh <bsbsharma47@gmail.com>
2021-03-14 14:38:36 +0530

    tested

--- downloader.py
+++ downloader.py
@@ -1,108 +1,116 @@
-form bs4 import *
+from bs4 import BeautifulSoup
 import requests 
-import os
+import os 
 
-#create forlder 
- def folder_create(images) :   #function
-     try:
-            folder_ name == input("Enter folder name :- ")
-            #folder creation 
-            os.mkdir(folder_name)
+# CREATE FOLDER 
+def folder_create(images): 
+	try: 
+		folder_name = input("Enter Folder Name:- ") 
+		
+		os.mkdir(folder_name) 
 
-    # if folder exists with that name , ask another name 
-    except:
-        print("folder exist with that name!")
-        folder_create()
+	# if folder exists with that name, ask another name 
+	except: 
+		print("Folder Exist with that name!") 
+		folder_create(images) 
 
-    #image downloading start 
-     download-image(image,folder_name)
+	# image downloading start 
+	download_images(images, folder_name) 
 
-#dowload all images from that url
-def download_images(images, folder_name):
 
-        #initial count is zero
-        count=0
-        #print total images found in url
-        print(f"Total {len(images)} Image found!")
+# DOWNLOAD ALL IMAGES FROM THAT URL 
+def download_images(images, folder_name): 
+	
+	# intitial count is zero 
+	count = 0
 
-        #checking if images is not zero
-        if len(images) != 0:
-            for i,image in enumerate(images):
-                #from image tag,fetch image dource url
-                 #data-srcset
-                 #data-src
-                 #data-fallback-src
-                 #src
-                #here we will use exceptional handling
-                # first we will search for "data-srcset" in img tag 
-                try:
-                    # in image tag, searching for "data-srcset"
-                    image_link = image["data-srcset"]
-                     # then we will search for "data-src" in img  
-                     # tag and so on.. 
-                     except:
-                         try:
-                              # In image tag ,searching for "data-src"
-                              image_link = image["data-src"]
-                         except:
-                             try:
-                                  # In image tag ,searching for "src" 
-                                    image_link = image["data-fallback-src"] 
-                             except: 
-                                try: 
-                                    # In image tag ,searching for "src" 
-                                    image_link = image["src"] 
-        
-                                # if no Source URL found 
-                                except: 
-                                    pass           
+	# print total images found in URL 
+	print(f"Total {len(images)} Image Found!") 
 
-            # After getting Image Source URL 
-            # We will try to get the content of image 
+	# checking if images is not zero 
+	if len(images) != 0: 
+		for i, image in enumerate(images): 
+			# From image tag ,Fetch image Source URL 
 
-             try:
-                 r = requests.get(image_link).content
-                 try:
-                     #responsibility of decode
-                     r = str(r,'utf-8')
+						# 1.data-srcset 
+						# 2.data-src 
+						# 3.data-fallback-src 
+						# 4.src 
 
-                 except UnicodeDecodeError:
-                     # After checking above condition, Image Download start 
-                     with open(f"{folder_name}/images{i+1}.jpg","wb+") as f:
-                        f.write(r)
- # counting number of image downloaded 
-                    count +=1
-            except:
-                pass
+			# Here we will use exception handling 
 
-       # There might be possible, that all 
-        # images not download 
-        # if all images download 
+			# first we will search for "data-srcset" in img tag 
+			try: 
+				# In image tag ,searching for "data-srcset" 
+				image_link = image["data-srcset"] 
+				
+			# then we will search for "data-src" in img 
+			# tag and so on.. 
+			except: 
+				try: 
+					# In image tag ,searching for "data-src" 
+					image_link = image["data-src"] 
+				except: 
+					try: 
+						# In image tag ,searching for "data-fallback-src" 
+						image_link = image["data-fallback-src"] 
+					except: 
+						try: 
+							# In image tag ,searching for "src" 
+							image_link = image["src"] 
 
-        if count == len(images):
-            print("All Images Downloaded!")
+						# if no Source URL found 
+						except: 
+							pass
 
-          # if all images not download   
-        else:
-            print(f"Total {count} Images Downloaded out of {len(images)}")
+			# After getting Image Source URL 
+			# We will try to get the content of image 
+			try: 
+				r = requests.get(image_link).content 
+				try: 
+
+					# possibility of decode 
+					r = str(r, 'utf-8') 
+
+				except UnicodeDecodeError: 
+
+					# After checking above condition, Image Download start 
+					with open(f"{folder_name}/images{i+1}.jpg", "wb+") as f: 
+						f.write(r) 
+
+					# counting number of image downloaded 
+					count += 1
+			except: 
+				pass
+
+		# There might be possible, that all 
+		# images not download 
+		# if all images download 
+		if count == len(images): 
+			print("All Images Downloaded!") 
+			
+		# if all images not download 
+		else: 
+			print(f"Total {count} Images Downloaded Out of {len(images)}") 
 
 # MAIN FUNCTION START 
-def main(url):
-    # content of URL 
-    r = request.get(url)
+def main(url): 
+	
+	# content of URL 
+	r = requests.get(url) 
 
-    #parse html code
-    soup = BeautifullSoup(r.text,'html.parse')
+	# Parse HTML Code 
+	soup = BeautifulSoup(r.text, 'html.parser') 
 
-    # find all images in URL 
-    images = soup.findAll('img')
+	# find all images in URL 
+	images = soup.findAll('img') 
 
-    #   # Call folder create function 
-    folder_create(images)
+	# Call folder create function 
+	folder_create(images) 
 
 
 # take url 
-url = imput("Enter URL :- ")
+url = input("Enter URL:- ") 
 
 # CALL MAIN FUNCTION 
-main(url)
+main(url) 
